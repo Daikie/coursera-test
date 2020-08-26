a1713fc81fb0cab589ce8edb0f82b3b21ab22d39
unknown <daikiekobayashi@hotmail.com>
2020-08-27 03:29:15 +0900

    test4

---
+++ site/js/script.js
@@ -0,0 +1,16 @@
+var string="Hello";
+string += " World";
+console.log(string +  "!");
+
+console.log(4*5);
+
+var x=2, y=3;
+if (x>y) {
+	console.log("large");
+} else {
+	console.log("small");
+}
+
+for (var i=1; i<10; i++) {
+	console.log(i);
+}
